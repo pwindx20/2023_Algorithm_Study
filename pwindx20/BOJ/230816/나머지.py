a3bd82310c7ec212d2a894f288747e4b87243ffb
kangjisoo <pwindx20@gmail.com>
2023-08-16 17:38:36 +0900

    ADD: [BOJ] 10171. 고양이

--- pwindx20/BOJ/230816/나머지.py
+++ pwindx20/BOJ/230816/나머지.py
@@ -3,6 +3,6 @@
 # 출력: (A+B)%C/ ((A%C)+(B%C))/ (A*B)%C/ ((A%C)*(B%C))%C
 A, B, C = map(int, input().split())
 print((A+B)%C)
-print(((A%C)+(B%C)))
+print(((A%C)+(B%C))%C)
 print((A*B)%C)
 print(((A%C)*(B%C))%C)

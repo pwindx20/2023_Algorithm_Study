e8650abb095832b1a5556feec043c5f6f5a34d26
kjsoo <pwindx20@gmail.com>
2023-08-17 23:37:49 +0900

    ADD: [SEA] 4408. 자기 방으로 돌아가기

--- pwindx20/SEA/230817/자기방으로돌아가기.py
+++ pwindx20/SEA/230817/자기방으로돌아가기.py
@@ -1,1 +1,13 @@
 # 4408 
+# 입력: T/ 학생수 N / 현재방, 돌아갈방 <=400 중복X
+# 출력: #t 전부 돌아가는 데 걸리는 시간
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    corridor = [0]*201
+    for _ in range(N):
+        s, e = sorted(list(map(int, input().split())))
+        for i in range((s+1)//2, (e+1)//2+1):
+            corridor[i] += 1
+    print(f'#{tc} {max(corridor)}')

2e45b50b72ab976240958f1310678cbe5fbecfc0
kangjisoo <pwindx20@gmail.com>
2023-08-17 15:30:12 +0900

    ADD: [BOJ] 25304. 영수증

--- pwindx20/BOJ/230817/영수증.py
+++ pwindx20/BOJ/230817/영수증.py
@@ -1,7 +1,11 @@
 # 25304
-# 입력 N 1~9
-# 출력 n*1 ~ n*9
+# 입력: 총금액/ 물건의 종류 수 / 가격a, 개수 b
+# 출력: 구매한 물건의 가격과 개수로 계산한 총 금액이 영수증의 총금액과 일치하면 Yes , 아니면 No
 
+X=int(input())
 N = int(input())
-for i in range(1, 10):
-    print(f'{N} * {i} = {N*i}')
+total = 0
+for _ in range(N):
+    a, b = map(int, input().split())
+    total += a*b
+print('Yes') if total == X else print('No')

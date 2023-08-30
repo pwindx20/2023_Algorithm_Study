320213db627aa3674ad03faca9f7529da8677ee7
kjsoo <pwindx20@gmail.com>
2023-08-30 23:54:15 +0900

    ADD: [BOJ] 10798. 세로읽기

--- pwindx20/BOJ/230830/세로읽기.py
+++ pwindx20/BOJ/230830/세로읽기.py
@@ -1,1 +1,8 @@
 # 10798
+s_lst = [input()+' '*15 for _ in range(5)]
+
+for i in range(15):
+    for j in range(5):
+        if s_lst[j][i] == ' ':
+            continue
+        print(s_lst[j][i], end='')

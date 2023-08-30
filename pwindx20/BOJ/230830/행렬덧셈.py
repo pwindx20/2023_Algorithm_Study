d5d56d6588d8e4a3cb99286f1995eaae103171b0
kjsoo <pwindx20@gmail.com>
2023-08-30 23:37:27 +0900

    ADD: [BOJ] 2738. 행렬덧셈

--- pwindx20/BOJ/230830/행렬덧셈.py
+++ pwindx20/BOJ/230830/행렬덧셈.py
@@ -1,1 +1,8 @@
 # 2738
+N, M = map(int, input().split())
+A = [list(map(int,input().split())) for _ in range(N)]
+B = [list(map(int,input().split())) for _ in range(N)]
+for n in range(N):
+    for m in range(M):
+        print(A[n][m] + B[n][m], end=' ')
+    print()

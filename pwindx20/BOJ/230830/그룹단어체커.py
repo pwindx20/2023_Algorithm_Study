33bab35c58e175d044b6b2347f9fb18ab01692a4
kjsoo <pwindx20@gmail.com>
2023-08-30 23:24:40 +0900

    ADD: [BOJ] 1316. 그룹 단어 체커

--- pwindx20/BOJ/230830/그룹단어체커.py
+++ pwindx20/BOJ/230830/그룹단어체커.py
@@ -1,1 +1,10 @@
 # 1316
+N = int(input())
+cnt = 0
+for _ in range(N):
+    s = input().strip()
+    for i in range(len(s)-1):
+        if s[i]!= s[i+1] and s[i+1:].count(s[i])>0:
+            break
+    else: cnt += 1
+print(cnt)

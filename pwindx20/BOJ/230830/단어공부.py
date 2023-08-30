c182ad1830bf0b1f0445666a6db92c92f62d050b
kjsoo <pwindx20@gmail.com>
2023-08-31 00:06:59 +0900

    MOD: [BOJ] 1157. 단어공부

--- pwindx20/BOJ/230830/단어공부.py
+++ pwindx20/BOJ/230830/단어공부.py
@@ -3,5 +3,12 @@
 # 출력: 가장 많이 사용된 알파벳을 대문자로 출력, 여러개일시 ? 출력
 S = list(input().upper())
 N = dict()
-for i in range(len(S)):
-    
+for s in set(S):
+    cnt = S.count(s)
+    if cnt in N:
+        print('?') # 반례 Cbtt 해결방안 고민중! 
+        break
+    else:
+        N[cnt]=s
+else:
+    print(N[max(N.keys())])

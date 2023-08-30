33fe92d6d0557c10e2107a5b0395edab2e77861c
kjsoo <pwindx20@gmail.com>
2023-08-30 23:14:35 +0900

    ADD: [BOJ] 2941. 크로아티아 알파벳

--- pwindx20/BOJ/230830/크로아티아알파벳.py
+++ pwindx20/BOJ/230830/크로아티아알파벳.py
@@ -1,1 +1,9 @@
 # 2941
+s= input().strip()
+alph = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+cnt = 0
+for a in alph:
+    cnt += s.count(a)
+    if a=='z=':
+        cnt -=s.count('dz=')
+print(len(s)-cnt-s.count('dz='))

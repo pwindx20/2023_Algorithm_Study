c474a1e9a8ce3e2ae28f5e84d39a2ecd4e0b54c1
kangjisoo <pwindx20@gmail.com>
2023-09-08 16:44:54 +0900

    MOD: [BOJ] 1934. 최소공배수

--- pwindx20/BOJ/230908/최소공배수.py
+++ pwindx20/BOJ/230908/최소공배수.py
@@ -1,7 +1,19 @@
 # 1934
 # 입력: T/ A, B
 # 출력: A와 B의 최소공배수
+import sys
+T= int(sys.stdin.readline())
+for _ in range(T):
+    A, B = map(int, sys.stdin.readline().split())
+    if A>B:
+        x, y = B, A
+    else: x, y = A, B
+        
+    # 유클리드 호제법
+    while y:
+        x, y = y, x % y
 
+    print(A*B//x)
 
 #########시간 초과###############
 # import sys

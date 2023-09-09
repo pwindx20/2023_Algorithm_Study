47f87ebf2a3556cdb6d0d25c6b5e3db2099aa94c
wjdxodbs <wjdxodbs52@naver.com>
2023-09-09 12:05:01 +0900

    ADD: [BOJ] 18870. 좌표 압축

---
+++ wjdxodbs/BOJ/20230907/좌표 압축.py
@@ -0,0 +1,9 @@
+from sys import stdin
+input = stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+arr2 = sorted(set(arr))
+dic = {arr2[i]: i for i in range(len(arr2))}
+for i in arr:
+    print(dic[i], end = ' ')

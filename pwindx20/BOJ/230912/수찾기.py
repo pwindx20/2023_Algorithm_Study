e86ac26bb6caaec2a7de22d03bbc5009b360f385
kangjisoo <pwindx20@gmail.com>
2023-09-13 13:45:31 +0900

    ADD: [BOJ] 4948. 베르트랑공준

---
+++ pwindx20/BOJ/230912/수찾기.py
@@ -0,0 +1,33 @@
+# 1920
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##############시간초과############
+# import sys
+# def solve(num):
+#     if num in A_lst:
+#         return 1
+#     return 0
+
+# N = int(sys.stdin.readline())
+# A_lst = list(map(int,sys.stdin.readline().split()))
+# A_lst.sort()
+# M = int(sys.stdin.readline())
+# B_lst = list(map(int,sys.stdin.readline().split()))
+# for n in B_lst:
+#     print(solve(n))

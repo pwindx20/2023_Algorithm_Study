c4fe2dde2b93fecbf781d950b529478f1721d1bd
kangjisoo <pwindx20@gmail.com>
2023-09-14 17:11:53 +0900

    ADD: [BOJ] 11866. 요세푸스문제0

---
+++ pwindx20/BOJ/230914/요세푸스문제0.py
@@ -0,0 +1,18 @@
+# 11866
+# 입력: N, K
+# 출력: 1~N번 원 K번째 사람을 제거 한 사람이 제거 되면 남은 사람들로 이루어진 원을 따라 이 과정을 계속해나간다.
+# N명의 사람이 모두 제거될 때까지 제거되는 순서를 순열이라고 한다
+
+N, K = map(int, input().split())
+lst = list(range(1, N+1))
+answer = []
+cnt = 1
+while lst:
+    v = lst.pop(0)
+    if cnt ==K:
+        answer.append(v)
+        cnt =1
+    else:
+        lst.append(v)
+        cnt += 1
+print('<'+', '.join(map(str, answer))+'>')
---
+++ pwindx20/BOJ/230914/카드2.py
@@ -0,0 +1,16 @@
+# 2164
+# 입력: N
+# 출력: 첫째 줄에 남게 되는 카드의 번호 출력
+from collections import deque
+
+N = int(input())
+
+
+
+
+########## 7일때 #################
+# N = int(input())
+# i = 0
+# while N>=(1<<i):
+#     i+=1
+# print(1<<(i-1))

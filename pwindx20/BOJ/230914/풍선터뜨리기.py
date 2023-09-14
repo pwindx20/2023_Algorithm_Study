132064d0c7adcaf9408525269b1dd503c467a974
kangjisoo <pwindx20@gmail.com>
2023-09-14 17:53:28 +0900

    ADD: [BOJ] 2346. 풍선 터뜨리기 ing

---
+++ pwindx20/BOJ/230914/풍선터뜨리기.py
@@ -0,0 +1,11 @@
+# 2346
+# 입력: N/ 풍선 안의 종이
+# 출력: 터진 풍선의 번호
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+b = [0]+list(map(int, sys.stdin.readline().split()))
+
+v = 1
+for i in range

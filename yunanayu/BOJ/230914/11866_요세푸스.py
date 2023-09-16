76a5925f3d9a3a9184a652dd4eabbaeb5955c4dd
yuna <jyn7077@naver.com>
2023-09-17 03:30:59 +0900

    ADD [BOJ] 211866_요세푸스

--- yunanayu/BOJ/230914/11866_요세푸스.py
+++ yunanayu/BOJ/230914/11866_요세푸스.py
@@ -1,5 +1,10 @@
+from collections import deque
 N, K = map(int,input().split())
-
 lst = [(i+1) for i in range(N)]
+Q = deque(lst)
 result = []
-# print(lst)
+while Q:
+    Q.rotate(-(K-1))
+    result.append(str(Q.popleft()))
+
+print('<'+', '.join(result)+'>')

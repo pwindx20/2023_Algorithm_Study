c4f84975c7a73c3a120605436d061ff00f2d5d04
wjdxodbs <wjdxodbs52@naver.com>
2023-10-05 21:40:20 +0900

    ADD: [BOJ] 10815. 숫자 카드

---
+++ wjdxodbs/BOJ/20231005/숫자 카드.py
@@ -0,0 +1,13 @@
+from collections import deque
+from sys import stdin
+input = stdin.readline
+
+N = int(input())
+arr = set(map(int, input().split()))
+M = int(input())
+lst = list(map(int, input().split()))
+for i in lst:
+    if i in arr:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')

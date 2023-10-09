161ab00478cb6f4cb4d15238a0bcd117e5431c12
yuna <jyn7077@naver.com>
2023-10-09 14:48:16 +0900

    ADD [BOJ] 10814_숫자카드

---
+++ yunanayu/BOJ/231004/1080_행렬.py
@@ -0,0 +1,5 @@
+N, M = map(int, input().split())
+A = [list(input()) for _ in range(N)]
+B = [list(input()) for _ in range(N)]
+print(A)
+print(B)
---
+++ yunanayu/BOJ/231005/10815_숫자카드.py
@@ -0,0 +1,34 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+def search(num):
+    start = 0
+    end = N-1
+    while start <= end:
+        middle = (start + end) // 2
+        if lst[middle] > num:
+            end = middle -1
+        elif lst[middle] < num:
+            start = middle + 1
+        else:
+            return 1
+    return 0
+
+
+N = int(input())
+lst = list(map(int, input().split()))
+M = int(input())
+M_lst = list(map(int, input().split()))
+lst.sort()
+for m in M_lst:
+    print(search(m), end=' ')
+
+
+
+# for m in M_lst:
+#     if m in lst:
+#         print(1, end=' ')
+#     else:
+#         print(0, end=' ')
+    # print(lst)
+# print(M_lst)
